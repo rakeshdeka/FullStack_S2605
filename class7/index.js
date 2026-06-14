acdfffaa29141ec5fe5ea88cef824a4aa50ad257
rakeshdeka <58238022+rakeshdeka@users.noreply.github.com>
2026-06-14 19:34:22 +0530

    added class6 and 7

---
+++ class7/index.js
@@ -0,0 +1,107 @@
+// console.log(2-1)
+// variable : to store data
+
+// var, let, const
+
+// var age=20;
+// age= 21;
+// var age=22;
+// console.log(age);
+
+
+
+// let name="Jhon";
+// name="Doe";
+// let name="Smith";
+// console.log(name);
+
+// const rollNo=101;
+// rollNo=102;
+// const rollNo=103;
+// console.log(rollNo);
+
+// Datatypes:
+
+// String
+
+// let name = "12";
+// console.log(name);
+
+// Number
+
+// let age = 20;
+// console.log(age);
+
+// Boolean
+
+// let isLoggedIn = false;
+// console.log(isLoggedIn);
+
+//Array
+
+// let fruits = ["Apple", "Banana", 12, 22, "Mango"];
+// console.log(fruits);
+
+//Object
+
+// let user={
+//     name:"Rakesh",
+//     age:25
+// };
+
+// console.log(user);
+
+// operators
+
+// Arithmetic operat0ors
+
+// var a=10;
+// var b=5;
+
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// comparison operators
+
+// > < == ===
+
+// console.log(10>5)
+// console.log(10<5);
+
+// let a=10
+// =
+
+// ==
+// ===
+
+// console.log(10 == "10"); //only checks value
+// console.log(10 === "10"); // both value and type
+
+// conditions
+
+// if, if else, nested if
+
+let age = 16;
+if(age>=18){
+
+    console.log("Yes! You can vote");
+    
+} else{
+    console.log("No You can't vote! Better luck next time");
+    
+}
+
+// 1. check if a number is postive
+// 2.check if number is negative
+// 3. check if a number is greater then 100
+
+
+
+
+
+
+
